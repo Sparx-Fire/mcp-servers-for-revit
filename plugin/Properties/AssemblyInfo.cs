1a52de93d9f4565f94d0d1541186c9cf18f3648f
Jean-Marc Couffin <jmcouffin@users.noreply.github.com>
2026-02-26 18:08:37 +0100

    1.0.0

--- plugin/Properties/AssemblyInfo.cs
+++ plugin/Properties/AssemblyInfo.cs
@@ -1,37 +1,37 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
-// 有关程序集的一般信息由以下
-// 控制。更改这些特性值可修改
-// 与程序集关联的信息。
+// ?????????????
+// ?????????????
+// ??????????
 [assembly: AssemblyTitle("mcp-servers-for-revit-plugin")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("P R C")]
 [assembly: AssemblyProduct("mcp-servers-for-revit-plugin")]
-[assembly: AssemblyCopyright("Copyright © P R C 2025")]
+[assembly: AssemblyCopyright("Copyright � P R C 2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 
-// 将 ComVisible 设置为 false 会使此程序集中的类型
-//对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型
-//请将此类型的 ComVisible 特性设置为 true。
+// ? ComVisible ??? false ??????????
+//? COM ??????????? COM ??????????
+//?????? ComVisible ????? true?
 [assembly: ComVisible(false)]
 
-// 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
+// ?????? COM ??,??? GUID ?????? ID
 [assembly: Guid("43cd0fd7-df41-4f64-92be-a0f78666d86f")]
 
-// 程序集的版本信息由下列四个值组成: 
+// ????????????????: 
 //
-//      主版本
-//      次版本
-//      生成号
-//      修订号
+//      ???
+//      ???
+//      ???
+//      ???
 //
-//可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
-//通过使用 "*"，如下所示:
-// [assembly: AssemblyVersion("1.0.*")]
+//?????????,?????�???�?�???�????
+//???? "*",????:
+// [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
